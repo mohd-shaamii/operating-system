1cae8f8e322dc1c13b41129f5e8ed66db6cd3dd1
HalalHacker <122071878+mohd-shaamii@users.noreply.github.com>
2023-07-10 14:13:09 +0530

    Update FCFS.c

--- Lab Programs/FCFS.c
+++ Lab Programs/FCFS.c
@@ -1,23 +1,17 @@
-#include&lt;stdio.h&gt;
+#include<stdio.h>
 int main()
 {
 char pn[10];
-int
-arr[10],bur[10],star[10],finish[10],tat[1
-0],wt[10],i,n;
+int arr[10],bur[10],star[10],finish[10],tat[10],wt[10],i,n;
 int totwt=0,tottat=0;
-printf(&quot;Enter the number of
-processes:&quot;);
-scanf(&quot;%d&quot;,&amp;n);
-for(i=0;i&lt;n;i++)
+printf("Enter the number of processes:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
 {
-printf(&quot;Enter the Process Name, Arrival
-Time &amp; Burst Time:&quot;);
-
-scanf(&quot;%s%d%d&quot;,&amp;pn[i],&amp;arr[i],&amp;bur[i])
-;
+printf("Enter the Process Name, Arrival Time & Burst Time:");
+scanf("%s%d%d",&pn[i],&arr[i],&bur[i]);
 }
-for(i=0;i&lt;n;i++)
+for(i=0;i<n;i++)
 {
 if(i==0)
 {
@@ -29,56 +23,40 @@ tat[i]=finish[i]-arr[i];
 else
 {
 star[i]=finish[i-1];
-
 wt[i]=star[i]-arr[i];
 finish[i]=star[i]+bur[i];
 tat[i]=finish[i]-arr[i];
 }
 }
-printf(&quot;\nPName Arrtime Burtime Start
-TAT Finish&quot;);
-for(i=0;i&lt;n;i++)
+printf("\nPName Arrtime Burtime Start TAT Finish");
+for(i=0;i<n;i++)
 {
-printf(&quot;\n%s\t%6d\t\t%6d\t%6d\t%6d\
-t%6d&quot;,pn[i],arr[i],bur[i],star[i],tat[i],fini
-sh[i]);
+printf("\n%s\t%6d\t\t%6d\t%6d\t%6d\t%6d",pn[i],arr[i],bur[i],star[i],tat[i],finish[i]);
 totwt+=wt[i];
 tottat+=tat[i];
 }
-
-printf(&quot;\nAverage Waiting time:%f&quot;,
-(float)totwt/n);
-printf(&quot;\nAverage Turn Around
-Time:%f&quot;, (float)tottat/n);
+printf("\nAverage Waiting time:%f", (float)totwt/n);
+printf("\nAverage Turn Around Time:%f", (float)tottat/n);
 }
-
-SOURCE CODE:
-/* A program to simulate the SJF CPU
-scheduling algorithm */
-#include&lt;stdio.h&gt;
-#include&lt;string.h&gt;
+#include<stdio.h>
+#include<string.h>
 main()
 {
-
-Int
-i=0,pno[10],bt[10],n,wt[10],temp=0,j,tt
-[10];
+int i=0,pno[10],bt[10],n,wt[10],temp=0,j,tt[10];
 float sum,at;
-printf(&quot;\n Enter the no of process &quot;);
-scanf(&quot; %d&quot;,&amp;n);
-printf(&quot;\n Enter the burst time of each
-process&quot;);
-for(i=0;i&lt;n;i++)
+printf("\n Enter the no of process ");
+scanf(" %d",&n);
+printf("\n Enter the burst time of each process");
+for(i=0;i<n;i++)
 {
-printf(&quot;\n p%d&quot;,i);
-scanf(&quot;%d&quot;,&amp;bt[i]);
+printf("\n p%d",i);
+scanf("%d",&bt[i]);
 }
-for(i=0;i&lt;n-1;i++)
+for(i=0;i<n-1;i++)
 {
-for(j=i+1;j&lt;n;j++)
-
+for(j=i+1;j<n;j++)
 {
-if(bt[i]&gt;bt[j])
+if(bt[i]>bt[j])
 {
 temp=bt[i];
 bt[i]=bt[j];
@@ -90,24 +68,18 @@ pno[j]=temp;
 }
 }
 wt[0]=0;
-for(i=1;i&lt;n;i++)
+for(i=1;i<n;i++)
 {
-
 wt[i]=bt[i-1]+wt[i-1];
 sum=sum+wt[i];
 }
-printf(&quot;\n process no \t burst time\t
-waiting time \t turn around time\n&quot;);
-for(i=0;i&lt;n;i++)
+printf("\n process no \t burst time\t waiting time \t turn around time\n");
+for(i=0;i<n;i++)
 {
 tt[i]=bt[i]+wt[i];
 at+=tt[i];
-printf(&quot;\n
-p%d\t\t%d\t\t%d\t\t%d&quot;,i,bt[i],wt[i],tt[
-i]);
+printf("\n p%d\t\t%d\t\t%d\t\t%d",i,bt[i],wt[i],tt[i]);
 }
-printf(&quot;\n\n\t Average waiting
-time%f\n\t Average turn around
-time%f&quot;, sum/n,at/n);
+printf("\n\n\t Average waiting time%f\n\t Average turn around time%f", sum/n,at/n);
+}
 
-}
