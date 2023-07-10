fd2e47dc28defa559a29d5071763c25d08949ba5
HalalHacker <122071878+mohd-shaamii@users.noreply.github.com>
2023-07-10 15:16:23 +0530

    Update Roundroin.c

--- Lab Programs/Roundroin.c
+++ Lab Programs/Roundroin.c
@@ -121,3 +121,35 @@ void main()
     scanf("%d",&quantum);
     roundRobin(processes,n,burst_time,quantum,arrival_time,prioritylist);
 }
+
+/*  OUTPUT:
+
+Total number of processes in the system: 4
+
+Enter the Arrival and Burst time of Process[1]
+Arrival time: 2
+Burst time: 1
+
+Enter the Arrival and Burst time of Process[2]
+Arrival time: 1
+Burst time: 5
+
+Enter the Arrival and Burst time of Process[3]
+Arrival time: 4
+Burst time: 1
+
+Enter the Arrival and Burst time of Process[4]
+Arrival time: 0
+Burst time: 6
+Enter the Time Quantum for the processes: 2
+
+Process No      Burst Time      TAT     Waiting Time
+Process No[1]   1               -1      -2
+Process No[3]   1               2       1
+Process No[2]   5               8       3
+Process No[4]   6               13      7
+
+Average Turn Around Time: 2.250000
+Average Waiting Time: 5.500000
+
+*/
