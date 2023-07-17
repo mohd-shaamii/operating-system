b4b9aacc832de50f5193e6cc8de2ecce0151d2ec
HalalHacker <122071878+mohd-shaamii@users.noreply.github.com>
2023-07-17 15:43:40 +0530

    Update philosephers.c

--- Lab Programs/philosephers.c
+++ Lab Programs/philosephers.c
@@ -84,3 +84,21 @@ void dine(int n)
   // Philosopher finishes eating
   printf("\nPhilosopher % d Finished eating ", n);
 }
+
+/*
+Philosopher  2 is thinking 
+Philosopher  2 is eating 
+Philosopher  4 is thinking 
+Philosopher  4 is eating 
+Philosopher  5 is thinking 
+Philosopher  5 is eating 
+Philosopher  1 is thinking 
+Philosopher  3 is thinking 
+Philosopher  4 Finished eating 
+Philosopher  3 is eating 
+Philosopher  1 is eating 
+Philosopher  5 Finished eating 
+Philosopher  2 Finished eating 
+Philosopher  1 Finished eating 
+Philosopher  3 Finished eating 
+*/
