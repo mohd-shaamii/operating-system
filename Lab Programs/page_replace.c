a268c80f90ef00dcd734a1a553dd16f700610d3e
HalalHacker <122071878+mohd-shaamii@users.noreply.github.com>
2023-07-24 14:44:46 +0530

    Update page_replace.c

--- Lab Programs/page_replace.c
+++ Lab Programs/page_replace.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+int main() {
+    int i, j, a[50], frame[10], no, k, avail, fault = 0;
+
+    printf("\n Enter the number of pages:\n");
+    scanf("%d", &no);
+
+    printf("\n Enter the page numbers:\n");
+    for (i = 0; i < no; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("\n Enter the number of frames:\n");
+    scanf("%d", &k);
+
+    for (i = 0; i < k; i++) {
+        frame[i] = -1;
+    }
+
+    j = 0;
+    printf("\nPage\t  Frames\n");
+
+    for (i = 0; i < no; i++) {
+        printf("%d\t", a[i]);
+        avail = 0;
+        for (j = 0; j < k; j++) {
+            if (frame[j] == a[i]) {
+                avail = 1;
+                break;
+            }
+        }
+
+        if (avail == 0) {
+            frame[fault % k] = a[i];
+            fault++;
+            for (j = 0; j < k; j++) {
+                printf("%d\t", frame[j]);
+            }
+            printf("\n");
+        }
+    }
+
+    printf("Total Page Faults: %d\n", fault);
+    return 0;
+}
